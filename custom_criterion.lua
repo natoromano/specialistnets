c464a09abc95e6d600e474f9b839ab56554ebced
Ubuntu <ubuntu@ip-172-31-13-65.us-west-1.compute.internal>
2016-03-09 05:05:18 +0000

    hotfixed bugs

--- custom_criterion.lua
+++ custom_criterion.lua
@@ -45,14 +45,14 @@ function DarkKnowledgeCriterion:updateOutput(input, target)
           self.output = self.kl_crit:forward(log_probs, soft_target)
       end
     else
-      local probs = self.sm:forward(input / div(self.temp))
+      local probs = self.sm:forward(input / self.temp)
       if self.supervised then
           self.output = self.ce_crit:forward(input, target.labels) * (1-self.alpha)
-          local str = string.format('CE/MSE loss: %1.0e/%1.0e', self.output,
-                             self.mse_crit:forward(probs, soft_target) * self.alpha) 
+          -- local str = string.format('CE/MSE loss: %1.0e/%1.0e', self.output,
+          --                   self.mse_crit:forward(probs, soft_target) * self.alpha) 
           self.output = self.output +
               self.mse_crit:forward(probs, soft_target) * self.alpha
-          print(str)
+          -- print(str)
       else
           self.output = self.mse_crit:forward(probs, soft_target)
       end
@@ -64,7 +64,7 @@ function DarkKnowledgeCriterion:updateGradInput(input, target)
     self.mask = target.labels:eq(0)
     local soft_target = self.sm:forward(target.scores:div(self.temp)):clone()
     if self.soft_loss == 'KL' then
-      local log_probs = self.lsm:forward(input / div(self.temp))
+      local log_probs = self.lsm:forward(input / self.temp)
       if self.supervised then
           local grad_ce = self.ce_crit:backward(input, 
                                       target.labels) * (1 - self.alpha)
@@ -91,8 +91,8 @@ function DarkKnowledgeCriterion:updateGradInput(input, target)
           grad_mse = self.sm:backward(input:div(self.temp),grad_mse) * (self.temp)
           -- grad_kl is multiplied by T^2 as recommended by Hinton et al. 
           self.gradInput = grad_ce + grad_mse
-          local str = string.format('CE/MSE grad:     %1.0e/%1.0e', grad_ce:norm(), grad_mse:norm())
-          print(str)
+          -- local str = string.format('CE/MSE grad:     %1.0e/%1.0e', grad_ce:norm(), grad_mse:norm())
+          -- print(str)
       else
           local grad_mse = self.mse_crit:backward(probs, soft_target)
           grad_mse = self.sm:backward(input:div(self.temp),grad_mse) * (self.temp)
