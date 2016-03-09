ffca0d2ab381d2ebcd8987e1b53d8810a33a92ae
Robin Schucker <schucker.robin@gmail.com>
2016-03-08 20:48:02 -0800

    fixed LOOOTS of torch arithmetic problem * temp instead of using :mul(temp)

--- custom_criterion.lua
+++ custom_criterion.lua
@@ -31,9 +31,9 @@ function DarkKnowledgeCriterion:updateOutput(input, target)
     -- input: raw scores from the model
     -- target.labels = ground truth labels
     -- target.scores = raw scores from the master
-    local soft_target = self.sm:forward(target.scores:div(self.temp))
+    local soft_target = self.sm:forward(target.scores / self.temp):clone()
     if self.soft_loss == 'KL' then
-      local log_probs = self.lsm:forward(input:div(self.temp))
+      local log_probs = self.lsm:forward(input / self.temp)
       if self.supervised then
           self.output = self.ce_crit:forward(input, target.labels) * (1-self.alpha)
           -- local str = string.format('CE/KL loss: %1.0e/%1.0e', self.output,
@@ -45,7 +45,7 @@ function DarkKnowledgeCriterion:updateOutput(input, target)
           self.output = self.kl_crit:forward(log_probs, soft_target)
       end
     else
-      local probs = self.sm:forward(input:div(self.temp))
+      local probs = self.sm:forward(input / div(self.temp))
       if self.supervised then
           self.output = self.ce_crit:forward(input, target.labels) * (1-self.alpha)
           local str = string.format('CE/MSE loss: %1.0e/%1.0e', self.output,
@@ -62,22 +62,22 @@ end
 
 function DarkKnowledgeCriterion:updateGradInput(input, target)
     self.mask = target.labels:eq(0)
-    local soft_target = self.sm:forward(target.scores:div(self.temp))
+    local soft_target = self.sm:forward(target.scores:div(self.temp)):clone()
     if self.soft_loss == 'KL' then
-      local log_probs = self.lsm:forward(input:div(self.temp))
+      local log_probs = self.lsm:forward(input / div(self.temp))
       if self.supervised then
           local grad_ce = self.ce_crit:backward(input, 
-                                      target.labels):mul(1 - self.alpha)
+                                      target.labels) * (1 - self.alpha)
           local grad_kl = self.kl_crit:backward(log_probs, 
-                                      soft_target):mul(self.alpha)
-          grad_kl = self.lsm:backward(input:div(self.temp),grad_kl):mul(self.temp)
+                                      soft_target) * (self.alpha)
+          grad_kl = self.lsm:backward(input:div(self.temp),grad_kl) * (self.temp)
           -- grad_kl is multiplied by T^2 as recommended by Hinton et al. 
           self.gradInput = grad_ce + grad_kl
           --local str = string.format('CE/KL grad:     %1.0e/%1.0e', grad_ce:norm(), grad_kl:norm())
           --print(str)
       else
           local grad_kl = self.kl_crit:backward(log_probs, soft_target)
-          grad_kl = self.lsm:backward(input:div(self.temp),grad_kl):mul(self.temp)
+          grad_kl = self.lsm:backward(input:div(self.temp),grad_kl) * (self.temp)
           -- grad_kl is multiplied by T^2 as recommended by Hinton et al. 
           self.gradInput = grad_kl        
       end
@@ -85,17 +85,17 @@ function DarkKnowledgeCriterion:updateGradInput(input, target)
       local probs = self.sm:forward(input:div(self.temp))
       if self.supervised then
           local grad_ce = self.ce_crit:backward(input, 
-                                      target.labels):mul(1 - self.alpha)
+                                      target.labels) * (1 - self.alpha)
           local grad_mse = self.mse_crit:backward(probs, 
-                                      soft_target):mul(self.alpha)
-          grad_mse = self.sm:backward(input:div(self.temp),grad_mse):mul(self.temp)
+                                      soft_target) * (self.alpha)
+          grad_mse = self.sm:backward(input:div(self.temp),grad_mse) * (self.temp)
           -- grad_kl is multiplied by T^2 as recommended by Hinton et al. 
           self.gradInput = grad_ce + grad_mse
           local str = string.format('CE/MSE grad:     %1.0e/%1.0e', grad_ce:norm(), grad_mse:norm())
           print(str)
       else
           local grad_mse = self.mse_crit:backward(probs, soft_target)
-          grad_mse = self.sm:backward(input:div(self.temp),grad_mse):mul(self.temp)
+          grad_mse = self.sm:backward(input:div(self.temp),grad_mse) * (self.temp)
           -- grad_kl is multiplied by T^2 as recommended by Hinton et al. 
           self.gradInput = grad_mse        
       end
